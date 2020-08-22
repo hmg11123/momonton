90f219834b343576a17660862668086801bf7924
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-08-22 14:25:19 +0900

    css

--- javascript/bg.js
+++ javascript/bg.js
@@ -1,23 +1,17 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBERT = 3;
-
-function handleImgLoad() {
-    console.log("finished loading");
-}
+const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
     const image = new Image();
     image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add("bgImage")
-    body.appendChild(image)
+    image.classList.add("bgImage");
+    body.prepend(image);
 }
 
-
 function genRandom() {
-    const number = Math.floor(Math.random() * 3);
+    const number = Math.floor(Math.random() * IMG_NUMBER);
     return number;
-
 }
 
 function init() {
